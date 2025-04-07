d51a2587e1dd062e2a32e707d9ccd7af986d6bc0
arvindr <arvindr@prl.res.in>
2025-04-07 11:14:17 +0530

    forms & settings

--- telescope_log/settings.py
+++ telescope_log/settings.py
@@ -166,8 +166,8 @@ LOGIN_URL = '/login/'
 
 # Automated email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST_DOMAIN")  # For Gmail
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # Store credentials in .env
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
